b9ddbdc0788a5aae6dd00e694efec7385b2c0b12
auychat <auychatcharin@gmail.com>
2023-03-30 20:40:10 +0700

    Add skeleton of login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,53 @@
-import Head from "next/head"
-import Layout from "@/components/layout/Layout"
+import Head from "next/head";
+import Layout from "@/components/layout/Layout";
+import Link from "next/link";
 
-export default function Login () {
-    return (
-        <Layout>
-            <Head>
-                <title>Login</title>
-            </Head>
-            <h1>Login</h1>
-        </Layout>
-    )
+export default function Login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <section className="w-3/4 mx-auto" flex flex-col gap-10>
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
+          <p className="w-3/4 mx-auto text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+            officia?
+          </p>
+        </div>
+
+        {/* form */}
+        <form className="flex flex-col gap-5">
+          <div className="input-group">
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="input-group">
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+
+          {/* login buttons */}
+          <div className="input-group">
+            <button type="submit">Login</button>
+          </div>
+
+          <div className="input-group">
+            <button type="submit">Sign In with Google</button>
+          </div>
+
+          <div className="input-group">
+            <button type="submit">Sign In with Github</button>
+          </div>
+        </form>
+
+        {/* bottom */}
+        <p className="text-center text-gray-400">
+          don{`'`}t have an account yet?
+          <Link href={"/register"} className="text-blue-700">
+            Sign Up
+          </Link>
+        </p>
+      </section>
+    </Layout>
+  );
 }
