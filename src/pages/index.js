45045ccf9f20980f6aab932a040a6beb6809a0f9
auychat <auychatcharin@gmail.com>
2023-04-03 16:05:33 +0700

    Protect Home Route
    หน้า Home จะสามารถเข้าได้เฉพาะตอน Login แล้วเท่านั้น หากไม่ Login จะ Redirect ไปหน้า Login เสมอ

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,25 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { getSession, useSession, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const {data: session} = useSession()
+  const { data: session } = useSession();
+
+  function handleSignOut() {
+    signOut();
+  }
 
   return (
     <>
       <Head>
         <title>Homepage | Next Auth</title>
       </Head>
-      <main className={styles.main}>{session ? User({session}) : Guest()}</main>
+      <main className={styles.main}>
+        {session ? User({ session, handleSignOut }) : Guest()}
+      </main>
     </>
   );
 }
@@ -38,7 +44,7 @@ function Guest() {
 }
 
 // Authorize User
-function User({session}) {
+function User({ session, handleSignOut }) {
   return (
     <main className="container mx-auto text-center py-20">
       <h3 className="text-4xl font-bold">Authorize User Homepage</h3>
@@ -49,7 +55,10 @@ function User({session}) {
       </div>
 
       <div className="flex justify-center">
-        <button className="mt-5 px-10 py-2 rounded-md bg-indigo-500 text-gray-50">
+        <button
+          onClick={handleSignOut}
+          className="mt-5 px-10 py-2 rounded-md bg-indigo-500 text-gray-50"
+        >
           Sign Out
         </button>
       </div>
@@ -64,4 +73,21 @@ function User({session}) {
       </div>
     </main>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
